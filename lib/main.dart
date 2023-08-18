3b17aecbd6cacacc0aa5e69b38611924802a4bfe
ferps53 <felipe.brostolin53@gmail.com>
2023-08-17 21:57:30 -0300

    Meals começa a ser passada por paramêtro!

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:navegando_entre_telas/data/dummy_data.dart';
 import 'package:navegando_entre_telas/screens/meal_detail_screen.dart';
 import 'package:navegando_entre_telas/screens/settings_screen.dart';
 import 'package:navegando_entre_telas/screens/tabs_screen.dart';
 import 'package:navegando_entre_telas/utils/app_routes.dart';
 import 'screens/categories_meals_screen.dart';
+import 'package:navegando_entre_telas/models/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Meal> _avaliableMeals = dummyMeals;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +36,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         AppRoutes.HOME: (context) => const TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (context) => const CategoriesMealsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (context) =>
+            CategoriesMealsScreen(_avaliableMeals),
         AppRoutes.MEAL_DETAIL: (context) => const MealDetailScreen(),
         AppRoutes.SETTINGS: (context) => const SettingsScreen(),
       },
